fed1ce5223847d695b3e865c347638b7720b12cf
Dev Rafael <devrafaael@gmail.com>
2025-05-30 22:33:04 -0300

    Enviando Mudancas

---
+++ portifolio/src/App.jsx
@@ -0,0 +1,222 @@
+
+import './App.css'
+import Apple from './assets/IMG/Apple.png';
+import Pizza from './assets/IMG/Pizza.png';
+import Casa from './assets/IMG/Casa.png';
+import MinhaFoto from './assets/IMG/MinhaFoto.jpg';
+import Linkedin from './assets/IMG/linkedin.png';
+import Whatsapp from './assets/IMG/whatsapp.png';
+import github from './assets/IMG/github.png';
+import css from './assets/IMG/css.png';
+import express from './assets/IMG/express.png';
+import figma from './assets/IMG/figma.png';
+import git from './assets/IMG/git.png';
+import html from './assets/IMG/html.png';
+import javascript from './assets/IMG/javascript.png';
+import node from './assets/IMG/node.png';
+import php from './assets/IMG/php.png';
+import react from './assets/IMG/react.png';
+import seta from './assets/IMG/seta.png';
+import sql from './assets/IMG/sql.png';
+import sol from './assets/IMG/sol.png';
+import lua from './assets/IMG/lua.png';
+import typescript from './assets/IMG/typescript.png';
+import videoFile from './assets/videoBackground.mp4';
+import { motion } from 'framer-motion';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+function App() {
+ useEffect(() => {
+  AOS.init();
+}, []);
+  return (
+    <>
+
+  <main>
+
+          <section className="videoSection">
+      <video autoPlay loop muted playsInline >
+        <source src={videoFile} type="video/mp4" />
+        Seu navegador não suporta o elemento de vídeo.
+      </video>
+      
+      <div className= "overlayGradient"></div>
+      
+      <article className="articleVideo">
+        <div className="estiloBackground">
+          <img src={sol} alt="Claro" /> 
+          <img src={lua} alt="Escuro" /> 
+      </div>
+        <span data-aos="fade-up" data-aos-delay="200">Olá! Sou o</span>
+      <h1 className="nome" data-aos="fade-down" data-aos-delay="200">Rafael Moraes</h1>
+       <article className="logos" data-aos="fade-down" data-aos-delay="200">
+          <a href="https://www.linkedin.com/in/rafael-moraes-13ba5b258/" target="_blank"><img src={Linkedin} alt="logo Linkedin"/></a>
+          <a href="tel:+5511916658824"><img src={Whatsapp} alt="logo telefone"/></a>
+          <a href="#"><img src={github} alt="logo email"/></a>
+          </article>
+          <motion.div
+  animate={{ y: [0, -10, 0] }}
+  transition={{ repeat: Infinity, duration: 1 }}
+>
+          <p> <img src={seta} alt="seta Para Baixo"/></p>
+          </motion.div>
+         <article class="quote-box" data-aos="fade-left" data-aos-delay="800">
+  <h3>Clean Code, Clear Purpose.</h3>
+  <p>— Chat GPT</p>
+</article>
+
+      </article>
+      
+    </section>
+</main>
+ 
+
+<section className="projetos" id="projetos">
+  {/* <div className="titleProjetos">
+    <h2>Projetos</h2>
+  <p>Uma seleção dos meus melhores trabalhos e projetos recentes</p>
+  </div> */}
+  <div className="title-wrapper" data-aos="fade-right" data-aos-delay="400" >
+    <h2>Projetos</h2>
+    <div className="line"></div>
+    
+  </div>
+<div className="subtitle" data-aos="fade-left" data-aos-delay="400">
+     <p>Uma seleção dos meus melhores trabalhos e projetos recentes</p>
+  </div>
+  <div className="content-projetos">
+
+    {/* Card Projeto */}
+    <div className="card" data-aos="fade-right" data-aos-delay="200">
+      <article className="cardResponsivo">
+        <div className="mockup">
+          <img src={Apple} className="device" alt="Notebook" />
+          </div>
+      </article>
+      <h5>Apple</h5>
+      <p>Clone da página de vendas do iPhone.</p>
+      <ul className="tecUsadas">
+        <li><img src={html} alt="HTML" /></li>
+        <li><img src={css} alt="CSS" /></li>
+        <li><img src={javascript} alt="JavaScript" /></li>
+      </ul>
+      <button><a href="https://dev-rafaael.github.io/VendasIPhone.github.io/" target="_blank">Ver Projeto</a></button>
+    </div>
+
+    {/* Card Projeto */}
+    <div className="card" data-aos="fade-left" data-aos-delay="200">
+      <article className="cardResponsivo">
+        <div className="mockup">
+          <img src={Pizza} className="device" alt="Notebook" />
+           </div>
+      </article>
+      <h5>La Pizza</h5>
+      <p>Landing Page de uma Pizzaria.</p>
+      <ul className="tecUsadas">
+        <li><img src={html} alt="HTML" /></li>
+        <li><img src={css} alt="CSS" /></li>
+      </ul>
+      <button><a href="https://dev-rafaael.github.io/PizzariaLaPizza.github.io/" target="_blank">Ver Projeto</a></button>
+    </div>
+
+    {/* Card Projeto */}
+ <div className="card" data-aos="fade-right" data-aos-delay="200">
+      <article className="cardResponsivo">
+        <div className="mockup">
+          <img src={Casa} className="device" alt="Notebook" />
+        </div>
+
+      </article>
+      <h5>DecoreHome</h5>
+      <p>Landing Page de evento de decoração de interiores.</p>
+      <ul className="tecUsadas">
+        <li><img src={html} alt="HTML" /></li>
+        <li><img src={css} alt="CSS" /></li>
+      </ul>
+      <button><a href="https://dev-rafaael.github.io/DecoreHome.github.io/" target="_blank">Ver Projeto</a></button>
+    </div>
+ {/* Card Projeto */}
+    <div className="card" data-aos="fade-left" data-aos-delay="200">
+      <article className="cardResponsivo">
+        <div className="mockup">
+          <img src={Casa} className="device" alt="Notebook" />
+        </div>
+
+      </article>
+      <h5>DecoreHome</h5>
+      <p>Landing Page de evento de decoração de interiores.</p>
+      <ul className="tecUsadas">
+        <li><img src={html} alt="HTML" /></li>
+        <li><img src={css} alt="CSS" /></li>
+      </ul>
+      <button><a href="https://dev-rafaael.github.io/DecoreHome.github.io/" target="_blank">Ver Projeto</a></button>
+    </div>
+  </div>
+</section>
+
+<section class="habilidadesContent" >
+   <div class="title-wrapper" data-aos="fade-right" data-aos-delay="400">
+      <h2>Habilidades</h2>
+      <div class="line"></div>
+    </div>
+    <div className="subtitle" data-aos="fade-left" data-aos-delay="400">
+     <p>Tecnologias e ferramentas que domino e utilizo nos meus projetos</p>
+  </div>
+  <article className='articleHabilidades' data-aos="fade-down" data-aos-delay="400">
+  <div className="listas">
+    <ul>
+      
+      <li>  <img src={react} alt="React JS"/></li>
+     <li>  <img src={javascript} alt="javascript"/></li>
+      <li>  <img src={typescript} alt="Typescript"/></li>
+     <li>  <img src={html} alt="HTML"/></li>
+      <li>  <img src={css} alt="CSS"/></li>
+       <li>  <img src={node} alt="Node JS"/></li>
+    </ul>
+    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, quisquam ad exercitationem facilis sit id, earum natus magnam iusto voluptates placeat fugiat voluptatum itaque doloremque totam amet molestias a assumenda.</span>
+  </div>
+   <div className="listas">
+     <ul>
+     
+      <li>  <img src={express} alt="Express JS"/></li>
+    
+ <li>  <img src={php} alt="PHP"/></li>
+    <li>  <img src={sql} alt="SQL"/></li>
+       <li>  <img src={git} alt="GIT"/></li>
+    <li>  <img src={figma} alt="Figma"/></li>
+
+ </ul>
+  <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, quisquam ad exercitationem facilis sit id, earum natus magnam iusto voluptates placeat fugiat voluptatum itaque doloremque totam amet molestias a assumenda.</span>
+  
+  </div>
+  </article>
+</section>
+ <section className="sobreMim" id="sobreMim" >    
+      <article className="contentSobre">
+      <div class="title-wrapperMim" data-aos="fade-right" data-aos-delay="400">
+        <h2><span>#</span>Sobre Mim</h2>
+        <div class="line"></div>
+    </div>
+    <article className="listaSobre" data-aos="fade-right" data-aos-delay="400">
+    <ul className='listaContent'>
+        <li>💻 Desenvolvedor Web com paixão por tecnologia e inovação 🚀</li>
+        <li>📍 Localizado em São Paulo, Brasil 🇧🇷</li>
+        <li>🎓 Graduado em Análise e Desenvolvimento de Sistemas pela Anhanguera Educacional</li>
+        <li>📚 Pós-graduando em Inteligência Artificial e Ciência de Dados pela Universidade Anhembi Morumbi 🤖📊</li>
+        <li>🧠 Participante ativo de diversos cursos de Desenvolvimento Web na plataforma Udemy 💡</li>
+      </ul>
+  <div className="btnSobreMim">
+     <button ><a href="#contato">Leia mais aqui</a></button>
+     </div>
+     </article>
+  </article>
+  <img src={MinhaFoto} data-aos="fade-left" data-aos-delay="400"/>
+</section>
+
+
+    </>
+  )
+}
+
+export default App
